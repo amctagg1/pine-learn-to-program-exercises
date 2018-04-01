14a8d18f03a9602fd3a57cc13477253eefcc236d
Abbey McTaggart <amctagg1@gmail.com>
2018-04-01 22:50:34 +0000

    complete chapter 2 exercises

---
+++ ch2/time_conversions.rb
@@ -0,0 +1,17 @@
+# Write a program that tells you the following:
+
+# How many hours are in a year?
+puts 'Hours in a year:'
+puts 1 * 365 * 24
+
+# How many minutes are in a decade?
+puts 'Minutes in a decade:'
+puts 1 * 10 * 365 * 24 * 60
+
+# How many seconds old are you?
+puts 'Seconds I have lived:'
+puts 30.7 * 365 * 24 * 60 * 60
+
+# If a person is 1,160 million seconds old - how old is this person in years?
+puts '1,160 million seconds in years:'
+puts (1160 * 1000000) / 60 / 60 / 24 / 365
